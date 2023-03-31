09b424573047598ce2ef5aba8d62656d2f45e5bb
CodeMyst <code@myst.rs>
2023-03-31 15:04:52 +0200

    feat: simple auth system

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:login_page, :login, :welcome]
+
+  def login_page
+  end
+
+  def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to '/welcome'
+  end
+
+  def welcome
+  end
+
+  def page_requires_login
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+
+  root 'sessions#welcome'
+
+  get 'login', to: 'sessions#login_page'
+  post 'login', to: 'sessions#login'
+  get 'logout', to: "sessions#logout"
+
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+end
